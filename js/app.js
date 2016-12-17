6a9c391ef8d03ad4bb8d7560211d7936034a9bf4
TJ Hopkins <thomas.john.hopkins@gmail.com>
2016-12-17 16:38:18 -0500

    Add scrolling and minor tweaks.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
 $(document).foundation();
+
+$("#button-one").click(function() {
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#section-one").offset().top + offset
+    }, 2000);
+});
+
+$("#button-two").click(function() {
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#section-two").offset().top + offset
+    }, 2000);
+});
+
+$("#button-three").click(function() {
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#section-three").offset().top + offset
+    }, 2000);
+});
+
+$("#button-four").click(function() {
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#section-four").offset().top + offset
+    }, 2000);
+});
+
+$("#button-five").click(function() {
+    var offset = 20; //Offset of 20px
+
+    $('html, body').animate({
+        scrollTop: $("#section-five").offset().top + offset
+    }, 2000);
+});
